97fc30919dde3669a49f4d7a5cbe68a830828885
Matam Rohith <143487715+Matam-Rohith@users.noreply.github.com>
2025-06-25 21:25:46 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -64,7 +64,7 @@ const Hero = () => {
               <Button 
                 variant="outline"
                 className="border-blue-600 text-blue-600 hover:bg-blue-50 dark:hover:bg-blue-900/20 px-5 py-2 rounded-full font-medium transition-all duration-300 hover:scale-105 text-sm"
-                onClick={() => window.open('https://drive.google.com/file/d/1vgDO3YO2rEnL5xV7ZVZlFvnhe7Leh7T4/view?usp=sharing', '_blank')}
+                onClick={() => window.open('https://drive.google.com/file/d/1iasT2idG_Sal1YDehiyxCiUo2Eqm4nvT/view?usp=drive_link', '_blank')}
               >
                 <Download className="w-4 h-4 mr-2" />
                 Download Resume
